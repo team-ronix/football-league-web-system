a3cbc5ddc33bcb83a66154cf2ab5f005b5ecbff4
Amr Mahmoud <amrmgoma6@gmail.com>
2025-12-08 08:18:15 +0200

    🐛fix: issue in CORS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
